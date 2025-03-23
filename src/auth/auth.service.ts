3f94a1680ddb5335ad6d53975bab64ee798396e1
Raen98 <david.fernandez1998.rd@gmail.com>
2025-03-23 17:22:11 +0100

    fly.io test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,73 +8,79 @@ import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-    private readonly configService: ConfigService
-  ) {}
+	constructor(
+		@InjectRepository(User)
+		private usersRepository: Repository<User>,
+		private readonly configService: ConfigService
+	) {}
 
-  async register(
-    email: string,
-    password: string,
-    name: string
-  ): Promise<{ message: string }> {
-    try {
-      const existingMail = await this.usersRepository.findOneBy({ email });
-      if (existingMail) {
-        return { message: 'email' };
-      }
-      const existingUser = await this.usersRepository.findOneBy({ name });
-      if (existingUser) {
-        return { message: 'user' };
-      }
+	async register(
+		email: string,
+		password: string,
+		name: string
+	): Promise<{ message: string }> {
+		try {
+			const existingMail = await this.usersRepository.findOneBy({
+				email,
+			});
+			if (existingMail) {
+				return { message: 'email' };
+			}
+			const existingUser = await this.usersRepository.findOneBy({ name });
+			if (existingUser) {
+				return { message: 'user' };
+			}
 
-      if (password.length < 0) {
-        return { message: 'password' };
-      }
+			if (password.length < 0) {
+				return { message: 'password' };
+			}
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+			const hashedPassword = await bcrypt.hash(password, 10);
 
-      const user = this.usersRepository.create({
-        email,
-        password: hashedPassword,
-        name,
-      });
-      await this.usersRepository.save(user);
-      return { message: '' };
-    } catch (error) {
-      return { message: (error as Error).message || 'Registration failed' };
-    }
-  }
+			const user = this.usersRepository.create({
+				email,
+				password: hashedPassword,
+				name,
+			});
+			await this.usersRepository.save(user);
+			return { message: '' };
+		} catch (error) {
+			return {
+				message: (error as Error).message || 'Registration failed',
+			};
+		}
+	}
 
-  async login(email: string, password: string): Promise<{ token: string }> {
-    // 1. Buscar usuario en la BD
-    const user = await this.usersRepository.findOneBy({ email });
-    if (!user) {
-      return { token: '' };
-    }
+	async login(email: string, password: string): Promise<{ token: string }> {
+		// 1. Buscar usuario en la BD
+		const user = await this.usersRepository.findOneBy({ email });
+		if (!user) {
+			return { token: '' };
+		}
 
-    // 2. Comparar contraseñas
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return { token: '' };
-    }
+		// 2. Comparar contraseñas
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) {
+			return { token: '' };
+		}
 
-    // 3. Generar JWT
-    const token = this.generateJwt(user);
+		// 3. Generar JWT
+		const token = this.generateJwt(user);
 
-    return { token };
-  }
+		return { token };
+	}
 
-  private generateJwt(user: User): string {
-    const secret = this.configService.get<string>('JWT_SECRET');
+	private generateJwt(user: User): string {
+		const secret = this.configService.get<string>('JWT_SECRET');
 
-    if (!secret) {
-      throw new Error('JWT_SECRET is not defined in the environment variables');
-    }
+		if (!secret) {
+			throw new Error(
+				'JWT_SECRET is not defined in the environment variables'
+			);
+		}
 
-    return jwt.sign({ id: user.id, email: user.email }, secret, {
-      expiresIn: '1h',
-    });
-  }
+		return jwt.sign({ id: user.id, email: user.email }, secret, {
+			//expiresIn: '15d',
+		});
+	}
 }
